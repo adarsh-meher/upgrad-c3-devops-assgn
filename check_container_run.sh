07c35710589229263df41d3b0068152c61477119
adarsh_github93 <adarsh.meher93@gmail.com>
2023-02-05 22:49:07 +0530

    code-check-container-running-status

--- check_container_run.sh
+++ check_container_run.sh
@@ -2,29 +2,40 @@
 
 echo "Check if we have any container running."
 
-CONT_STATIC_NAME="node_app"
-ECR_REPO_LINK = "262324735239.dkr.ecr.us-east-1.amazonaws.com"
-ECR_REPO_NAME = "assgn-c3-repo"
+CONT_STATIC_NAME="node-app"
+ECR_REPO_LINK="262324735239.dkr.ecr.us-east-1.amazonaws.com"
+ECR_REPO_NAME="assgn-c3-repo"
 
 
 
-x=$(docker ps -aqf "name=${CONT_STATIC_NAME}")
-null_check = if [ -z "$x" ];then echo "Null";else echo "Not Null";fi
+x=$(docker ps -aqf "name=$CONT_STATIC_NAME")
+echo "$x"
 
-if [ $null_check -eq "Null" ]
+
+if [ -z "$x" ]
+then
+        null_check="Null"
+else
+        null_check="Not Null"
+fi
+
+
+if [ "$null_check"=="Null" ]
 then 
     echo "No container running."
     echo "Starting new docker container"
-    docker run -itd -p 8081:8081 --name $CONT_STATIC_NAME  ${ECR_REPO_LINK}/${ECR_REPO_NAME}:v${BUILD_NUMBER}
+    docker run -itd -p 8081:8081 --name $CONT_STATIC_NAME $(docker images  -aq ${ECR_REPO_LINK}/${ECR_REPO_NAME} | head -n 1)
     echo "Container started."
 fi
 
-if [ $null_check -eq "Not Null" ]
+
+if [ "$null_check"=="Not Null" ]
 then
     echo "Some container running."
     echo "Stopping the container."
     docker stop $x
     docker rm $x
     echo "Starting new docker container"
-    docker run -itd -p 8081:8081 --name $CONT_STATIC_NAME  ${ECR_REPO_LINK}/${ECR_REPO_NAME}:v${BUILD_NUMBER}
+    docker run -itd -p 8081:8081 --name $CONT_STATIC_NAME  $(docker images  -aq ${ECR_REPO_LINK}/${ECR_REPO_NAME} | head -n 1)
     echo "Container started."
+fi
